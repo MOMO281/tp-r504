31922a3fb5c8e829ecd463b664df188b08322084
MOMO281 <morgan.moreau@univ-rouen.fr>
2024-09-11 11:25:35 +0200

    md

--- TP1/test.py
+++ TP1/test.py
@@ -6,3 +6,7 @@ def test_1 ( ) :
 def test_2():
 	assert f.puissance(2, -3) == 0.125
 	assert f.puissance(-2, 2) == 4
+	assert f.puissance(0.5, 3) == 0.125
+	assert f.puissance(1, -1) == 1
+	assert f.puissance(-1, 1) == -1
+	assert f.puissance(-1, -1) == -1
