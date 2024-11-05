207b4106899a4a8be900f5e80f523e8c930671a8
MOMO281 <morgan.moreau@univ-rouen.fr>
2024-11-05 11:23:41 +0100

    oui

---
+++ tp9/ClientTCP1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+
+public class ClientTCP1
+{
+	public static void main ( String [ ] args )
+	{	
+		try
+		{
+		Socket socket = new Socket("localhost", 2016);
+		DataOutpuStream dOUt = new DataOutputStream(socket.getOutputStream());
+		dOut.writeUTF("message de test");
+		socket.close();
+		}
+		catch (Exception ex)
+		{
+			System.out.println("Erreur !");
+		}
+	}
+}
