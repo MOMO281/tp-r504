f8e0d5095477bd37d06a700ecd74f0c59d2cbfad
MOMO281 <morgan.moreau@univ-rouen.fr>
2025-03-06 11:21:35 +0100

    carre

---
+++ nmap/scan_A106_v2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "# - IP - TCP ports - UDP ports" > "scan-result_2.csv"
+
+for ip in $(nmap -sn 172.16.0.0/24 | grep "Nmap scan report for" | awk '{print $5}'); do
+  open_tcp_ports=$(nmap --open -T4 -sT $ip | grep "^80/tcp" -c)
+  open_udp_ports=$(nmap --open -T4 -sU $ip | grep "^80/udp" -c)
+  echo "$ip;$open_tcp_ports;$open_udp_ports" >> "scan-result_2.csv"
+done
+
+echo "Scan terminé. Voir scan-result_2.csv."
